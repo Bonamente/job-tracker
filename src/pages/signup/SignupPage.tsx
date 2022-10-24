b2cafe7d543e4277e5fa2ec49c6efcc17920a3f8
Bonamente <56506035@protonmail.com>
2022-10-24 14:36:13 +0500

    feat: add SignupPage

---
+++ src/pages/signup/SignupPage.tsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import StyledSignupPage from './StyledSignupPage';
+import Logo from '../../components/logo/Logo';
+import Input from '../../components/input/Input';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { signUpUser, signInUser } from '../../features/user/userThunks';
+
+type InitialState = {
+  name: string;
+  email: string;
+  password: string;
+  isMember: boolean;
+};
+
+const initialState: InitialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
+const SignupPage = () => {
+  const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useAppSelector((store) => store.user);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }
+  }, [user, navigate]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('Please fill out all fields');
+      return;
+    }
+
+    if (isMember) {
+      dispatch(signInUser({ email, password }));
+      return;
+    }
+
+    dispatch(signUpUser({ name, email, password }));
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <StyledSignupPage className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h1>{values.isMember ? 'Sign in' : 'Sign up'} </h1>
+
+        {!values.isMember && (
+          <Input
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        <Input
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <Input
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button className="btn btn-block" type="submit" disabled={isLoading}>
+          {isLoading ? 'loading...' : 'submit'}
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button className="member-btn" type="button" onClick={toggleMember}>
+            {values.isMember ? 'Sign Up ' : 'Sign In'}
+          </button>
+        </p>
+      </form>
+    </StyledSignupPage>
+  );
+};
+
+export default SignupPage;
---
+++ src/pages/signup/StyledSignupPage.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const StyledSignupPage = styled.main`
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    margin: 0;
+    margin-top: 1rem;
+
+    text-align: center;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+
+    background: transparent;
+    border: transparent;
+  }
+`;
+
+export default StyledSignupPage;
