3e26a18b0672082e52dcc320841a632ed2c30556
Bonamente <56506035@protonmail.com>
2022-10-24 14:20:29 +0500

    feat: add functions to work with local storage

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,15 @@
+import { FetchedUser } from '../types';
+
+export const addUserToLocalStorage = (user: FetchedUser) => {
+  localStorage.setItem('user', JSON.stringify(user));
+};
+
+export const removeUserFromLocalStorage = () => {
+  localStorage.removeItem('user');
+};
+
+export const getUserFromLocalStorage = () => {
+  const result = localStorage.getItem('user');
+  const user = result ? JSON.parse(result) : null;
+  return user;
+};
