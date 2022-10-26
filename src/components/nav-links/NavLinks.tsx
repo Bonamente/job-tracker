633d4a0d112c43cdc75ecb3450c22f37ec969908
Bonamente <56506035@protonmail.com>
2022-10-26 13:23:10 +0500

    feat: add NavLinks component

---
+++ src/components/nav-links/NavLinks.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
+import links from './links';
+
+type NavLinksProps = {
+  toggleSidebar: () => void;
+};
+
+const NavLinks: React.FC<NavLinksProps> = ({ toggleSidebar }) => {
+  return (
+    <div className="nav-links">
+      {links.map(({ text, path, id, icon }) => (
+        <NavLink
+          key={id}
+          to={path}
+          className={({ isActive }) => {
+            return isActive ? 'nav-link active' : 'nav-link';
+          }}
+          onClick={toggleSidebar}
+        >
+          <span className="icon">{icon}</span>
+          {text}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default NavLinks;
---
+++ src/components/nav-links/links.tsx
@@ -0,0 +1,20 @@
+import { IoBarChartSharp } from 'react-icons/io5';
+import { MdQueryStats } from 'react-icons/md';
+import { FaWpforms } from 'react-icons/fa';
+import { ImProfile } from 'react-icons/im';
+
+type Link = {
+  id: number;
+  text: string;
+  path: string;
+  icon: React.ReactElement;
+};
+
+const links: Link[] = [
+  { id: 1, text: 'stats', path: '/', icon: <IoBarChartSharp /> },
+  { id: 2, text: 'all jobs', path: 'all-jobs', icon: <MdQueryStats /> },
+  { id: 3, text: 'add job', path: 'add-job', icon: <FaWpforms /> },
+  { id: 4, text: 'profile', path: 'profile', icon: <ImProfile /> },
+];
+
+export default links;
