fa82f35f2693c24de7351d7cb10420b07d645266
Bonamente <56506035@protonmail.com>
2022-10-29 14:20:34 +0500

    refactor: Input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,33 +1,31 @@
 type InputProps = {
+  labelText?: string; // Specify later
   type: string;
   name: string;
   value: string;
   handleChange(e: React.FormEvent<HTMLInputElement>): void;
-  labelText?: string; // Specify later
 };
 
 const Input: React.FC<InputProps> = ({
+  labelText,
   type,
   name,
   value,
   handleChange,
-  labelText,
-}) => {
-  return (
-    <div className="form-row">
-      <label className="form-label" htmlFor={name}>
-        {labelText || name}
-      </label>
-      <input
-        className="form-input"
-        id={name}
-        name={name}
-        value={value}
-        type={type}
-        onChange={handleChange}
-      />
-    </div>
-  );
-};
+}) => (
+  <div className="form-row">
+    <label className="form-label" htmlFor={name}>
+      {labelText || name}
+    </label>
+    <input
+      className="form-input"
+      id={name}
+      name={name}
+      value={value}
+      type={type}
+      onChange={handleChange}
+    />
+  </div>
+);
 
 export default Input;
