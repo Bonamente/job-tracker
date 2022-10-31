e2671d99fdeaf7e624ac01f3927a4770a5075073
Bonamente <56506035@protonmail.com>
2022-10-31 13:34:48 +0500

    feat(jobSlice): add createJob thunk

--- src/features/job/jobSlice.ts
+++ src/features/job/jobSlice.ts
@@ -1,8 +1,38 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-// import { toast } from 'react-toastify';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
-import { JobState } from '../../types';
-// import { getUserFromLocalStorage } from '../../utils/localStorage';
+import { JobState, Job, State, CustomFetchError } from '../../types';
+import { getUserFromLocalStorage } from '../../utils/localStorage';
+import customFetch from '../../utils/axios';
+
+// THUNKS are defined in this file because of the circular dependency (clearValues).
+export const createJob = createAsyncThunk<
+  Job, // replace with FetchedJob { Job + _id, createdBy, createdAt, updatedAt }
+  Job,
+  {
+    state: State;
+    rejectValue: string;
+  }
+>('job/createJob', async (job, thunkApi) => {
+  try {
+    const res = await customFetch.post('/jobs', job, {
+      headers: {
+        authorization: `Bearer ${thunkApi.getState().user.user?.token}`,
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    thunkApi.dispatch(clearValues());
+    return res.data;
+  } catch (error) {
+    const hasErrResponse = (error as CustomFetchError).response.data.msg;
+    if (!hasErrResponse) {
+      throw error;
+    }
+
+    return thunkApi.rejectWithValue(hasErrResponse);
+  }
+});
 
 const initialState: JobState = {
   isLoading: false,
@@ -37,11 +67,25 @@ const jobSlice = createSlice({
     clearValues: () => {
       return {
         ...initialState,
+        jobLocation: getUserFromLocalStorage()?.location || '',
       };
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createJob.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createJob.fulfilled, (state) => {
+        state.isLoading = false;
+        toast.success('Job Created');
+      })
+      .addCase(createJob.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
   },
 });
 
 export const { clearValues, handleChange } = jobSlice.actions;
-
 export default jobSlice.reducer;
