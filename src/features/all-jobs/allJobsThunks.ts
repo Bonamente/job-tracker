692060e5fa70fdfefed80b75ff7a6757fd6ee09d
Bonamente <56506035@protonmail.com>
2022-11-01 15:39:43 +0500

    feat(features/all-jobs): add getAllJobs thunk

---
+++ src/features/all-jobs/allJobsThunks.ts
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import customFetch from '../../utils/axios';
+import { State, CustomFetchError } from '../../types';
+
+type FetchedJob = {
+  _id: string;
+  company: string;
+  createdAt: string;
+  jobLocation: string;
+  jobType: string;
+  position: string;
+  status: string;
+};
+
+type FetchedJobs = {
+  jobs: FetchedJob[] | [];
+  totalJobs: number;
+  numOfPages: number;
+};
+
+export const getAllJobs = createAsyncThunk<
+  FetchedJobs,
+  void,
+  { state: State; rejectValue: string }
+>('allJobs/getJobs', async (_, thunkApi) => {
+  const url = `/jobs`;
+
+  try {
+    const res = await customFetch.get(url, {
+      headers: {
+        authorization: `Bearer ${thunkApi.getState().user.user?.token}`,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    const hasErrResponse = (error as CustomFetchError).response.data.msg;
+    if (!hasErrResponse) {
+      throw error;
+    }
+
+    return thunkApi.rejectWithValue(hasErrResponse);
+  }
+});
+
+export const anotherThunk = () => {};
