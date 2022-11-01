a18d73b21e4daf70ad961c801ec4061341b2cd7a
Bonamente <56506035@protonmail.com>
2022-11-01 15:30:11 +0500

    feat: update types

--- src/types.ts
+++ src/types.ts
@@ -43,6 +43,44 @@ export type JobState = {
   editJobId: string;
 };
 
+export type Job = {
+  position: string;
+  company: string;
+  jobLocation: string;
+  jobType: string;
+  status: string;
+};
+
+export type FetchedJob = {
+  _id: string;
+  company: string;
+  createdAt: string;
+  jobLocation: string;
+  jobType: string;
+  position: string;
+  status: string;
+};
+
+export type SortOptions = ['latest', 'oldest', 'a-z', 'z-a'];
+
+export type FiltersState = {
+  search: string;
+  searchStatus: string;
+  searchType: string;
+  sort: string;
+  sortOptions: SortOptions;
+};
+
+export type AllJobsState = {
+  isLoading: boolean;
+  jobs: FetchedJob[] | [];
+  totalJobs: number;
+  numOfPages: number;
+  page: number;
+} & FiltersState;
+
 export type State = {
   user: UserState;
+  job: JobState;
+  allJobs: AllJobsState;
 };
