ac51e41bb63ae895bc7885a1da8447357d4dc03f
Bonamente <56506035@protonmail.com>
2022-11-01 15:44:30 +0500

    feat(features/all-jobs): add allJobsSlice

---
+++ src/features/all-jobs/allJobsSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+import { getAllJobs } from './allJobsThunks';
+import { FiltersState, AllJobsState } from '../../types';
+
+const initialFiltersState: FiltersState = {
+  search: '',
+  searchStatus: 'all',
+  searchType: 'all',
+  sort: 'latest',
+  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+};
+
+const initialState: AllJobsState = {
+  isLoading: false,
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
+  ...initialFiltersState,
+};
+
+const allJobsSlice = createSlice({
+  name: 'allJobs',
+  initialState,
+  reducers: {
+    showLoading: (state) => {
+      state.isLoading = true;
+    },
+    hideLoading: (state) => {
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllJobs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllJobs.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.jobs = payload.jobs;
+      })
+      .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
+});
+
+export const { showLoading, hideLoading } = allJobsSlice.actions;
+
+export default allJobsSlice.reducer;
