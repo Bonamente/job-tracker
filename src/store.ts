3f6105b02de321c51df885afbb9ad5c5f7698b2c
Bonamente <56506035@protonmail.com>
2022-11-02 17:45:35 +0500

    feat(store): add allJobsReducer

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
 import jobReducer from './features/job/jobSlice';
+import allJobsReducer from './features/all-jobs/allJobsSlice';
 
 export const store = configureStore({
   reducer: {
-    job: jobReducer,
     user: userReducer,
+    job: jobReducer,
+    allJobs: allJobsReducer,
   },
 });
 
