7260f4309c00a89c88a2c54af91e96acfcabfa74
Bonamente <56506035@protonmail.com>
2022-11-03 15:07:04 +0500

    feat: add Job component

---
+++ src/components/job/Job.tsx
@@ -0,0 +1,73 @@
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+import { FetchedJob } from '../../types';
+import StyledJob from './StyledJob';
+import JobInfo from '../job-info/JobInfo';
+import { useAppDispatch } from '../../hooks';
+import { deleteJob, setEditJob } from '../../features/job/jobSlice';
+
+const Job: React.FC<FetchedJob> = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  status,
+}) => {
+  const dispatch = useAppDispatch();
+  const date = moment(createdAt).format('MMM Do, YYYY');
+
+  return (
+    <StyledJob>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h3>{position}</h3>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content">
+        <div className="content-center">
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${status}`}>{status}</div>
+        </div>
+        <footer>
+          <div className="actions">
+            <Link
+              className="btn edit-btn"
+              to="/add-job"
+              onClick={() =>
+                dispatch(
+                  setEditJob({
+                    editJobId: _id,
+                    position,
+                    company,
+                    jobLocation,
+                    jobType,
+                    status,
+                  })
+                )
+              }
+            >
+              Edit
+            </Link>
+            <button
+              type="button"
+              className="btn delete-btn"
+              onClick={() => dispatch(deleteJob(_id))}
+            >
+              delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </StyledJob>
+  );
+};
+
+export default Job;
---
+++ src/components/job/StyledJob.ts
@@ -0,0 +1,130 @@
+import styled from 'styled-components';
+
+const StyledJob = styled.article`
+  display: grid;
+  grid-template-rows: 1fr auto;
+
+  background-color: var(--white);
+  border-radius: var(--borderRadius);
+  box-shadow: var(--shadow-2);
+
+  header {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    padding: 1rem 1.5rem;
+
+    border-bottom: 1px solid var(--grey-100);
+
+    h3 {
+      letter-spacing: 0;
+    }
+  }
+
+  .main-icon {
+    display: grid;
+    place-items: center;
+    width: 60px;
+    height: 60px;
+    margin-right: 2rem;
+
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: var(--white);
+    text-transform: uppercase;
+
+    background-color: var(--primary-500);
+    border-radius: var(--borderRadius);
+  }
+
+  .info {
+    h3 {
+      margin-bottom: 0.25rem;
+    }
+
+    p {
+      margin: 0;
+
+      color: var(--grey-400);
+      text-transform: capitalize;
+      letter-spacing: var(--letterSpacing);
+    }
+  }
+
+  .pending {
+    color: #e9b949;
+    background-color: #fcefc7;
+  }
+
+  .interview {
+    color: #647acb;
+    background-color: #e0e8f9;
+  }
+
+  .declined {
+    color: #d66a6a;
+    background-color: #ffeeee;
+  }
+
+  .content {
+    padding: 1rem 1.5rem;
+  }
+
+  .content-center {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 0.5rem;
+
+    @media (min-width: 576px) {
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 992px) {
+      grid-template-columns: 1fr;
+    }
+
+    @media (min-width: 1120px) {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  .status {
+    width: 100px;
+    height: 30px;
+    margin-top: 0.5rem;
+
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    text-align: center;
+
+    border-radius: var(--borderRadius);
+  }
+
+  footer {
+    margin-top: 1rem;
+  }
+
+  .edit-btn,
+  .delete-btn {
+    height: 30px;
+    letter-spacing: var(--letterSpacing);
+    cursor: pointer;
+  }
+
+  .edit-btn {
+    margin-right: 0.5rem;
+    color: var(--green-dark);
+    background-color: var(--green-light);
+  }
+
+  .delete-btn {
+    color: var(--red-dark);
+    background-color: var(--red-light);
+  }
+
+  &:hover .actions {
+    visibility: visible;
+  }
+`;
+
+export default StyledJob;
