53171af557370392f7c64c9d63b8b4da7a002eab
Bonamente <56506035@protonmail.com>
2022-11-05 12:30:24 +0500

    feat: add GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+import { LightTheme, DarkTheme } from '../themes';
+
+type GlobalStylesProps = {
+  theme: LightTheme | DarkTheme;
+};
+
+const GlobalStyles = createGlobalStyle<GlobalStylesProps>`
+  body {
+    background-color: ${({ theme }) => theme.bodyBackground};
+    color: ${({ theme }) => theme.text};
+  }
+`;
+
+export default GlobalStyles;
