39c8e27af69ba9d6e5d3a4f568b21aa28bfaa17f
Bonamente <56506035@protonmail.com>
2022-11-06 14:35:36 +0500

    feat: add axios interceptor to customFetch

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,28 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable no-param-reassign */
 import axios from 'axios';
+import { getUserFromLocalStorage } from './localStorage';
 
 const customFetch = axios.create({
   baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+});
+
+customFetch.interceptors.request.use((config) => {
+  const user = getUserFromLocalStorage();
+
+  if (!config) {
+    config = {};
+  }
+
+  if (!config.headers) {
+    config.headers = {};
+  }
+
+  if (user) {
+    config.headers['Authorization'] = `Bearer ${user.token}`;
+  }
+
+  return config;
 });
 
 export default customFetch;
