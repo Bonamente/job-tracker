dac9188006c8a1a41ca7793bd5a02969503e492e
Bonamente <56506035@protonmail.com>
2022-11-09 11:36:07 +0500

    feat: add AreaChart

---
+++ src/components/area-chart/AreaChart.tsx
@@ -0,0 +1,31 @@
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+
+import { Application } from '../../types';
+
+type AreaChartComponentProps = {
+  data: Application[];
+};
+
+const AreaChartComponent: React.FC<AreaChartComponentProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#1e3a8a" fill="#3b82f6" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default AreaChartComponent;
