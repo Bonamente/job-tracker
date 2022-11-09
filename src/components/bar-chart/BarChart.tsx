f24c30e8873aa5f5a9b2c0e88d127f948118f919
Bonamente <56506035@protonmail.com>
2022-11-09 11:41:09 +0500

    feat: add BarChart

---
+++ src/components/bar-chart/BarChart.tsx
@@ -0,0 +1,31 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+
+import { Application } from '../../types';
+
+type BarChartComponentProps = {
+  data: Application[];
+};
+
+const BarChartComponent: React.FC<BarChartComponentProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="10 10 " />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#3b82f6" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarChartComponent;
