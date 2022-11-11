79ffde7c3c4708fc4dbfddc150c376cdcad1a0c4
Bonamente <56506035@protonmail.com>
2022-11-11 10:14:14 +0500

    feat: add SearchContainer

---
+++ src/components/search-container/SearchContainer.tsx
@@ -0,0 +1,82 @@
+import { AllJobsState, EventElement } from '../../types';
+import Input from '../input/Input';
+import Select from '../select/Select';
+import StyledSearchContainer from './StyledSearchContainer';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
+import {
+  handleChange,
+  clearFilters,
+} from '../../features/all-jobs/allJobsSlice';
+
+const SearchContainer = () => {
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useAppSelector((store) => store.allJobs);
+
+  const { jobTypeOptions, statusOptions } = useAppSelector(
+    (store) => store.job
+  );
+
+  const dispatch = useAppDispatch();
+
+  const handleSearch = (e: React.SyntheticEvent) => {
+    const target = e.target as EventElement;
+    const name = target.name as keyof AllJobsState;
+    const { value } = target;
+
+    dispatch(handleChange({ name, value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(clearFilters());
+  };
+
+  return (
+    <StyledSearchContainer>
+      <form className="form">
+        <h2>Search form</h2>
+        <div className="form-center">
+          <Input
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+
+          <Select
+            name="searchStatus"
+            labelText="status"
+            value={searchStatus}
+            list={['all', ...statusOptions]}
+            handleChange={handleSearch}
+          />
+
+          <Select
+            name="searchType"
+            labelText="type"
+            value={searchType}
+            list={['all', ...jobTypeOptions]}
+            handleChange={handleSearch}
+          />
+
+          <Select
+            name="sort"
+            value={sort}
+            list={sortOptions}
+            handleChange={handleSearch}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </StyledSearchContainer>
+  );
+};
+
+export default SearchContainer;
---
+++ src/components/search-container/StyledSearchContainer.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+const StyledSearchContainer = styled.section`
+  .form {
+    width: 100%;
+    max-width: 100%;
+
+    background-color: ${(props) => props.theme.secondaryBackground};
+  }
+
+  .form-input,
+  .form-select,
+  .btn-block {
+    height: 35px;
+  }
+
+  .form-row {
+    margin-bottom: 0;
+  }
+
+  .form-center {
+    display: grid;
+    grid-template-columns: 1fr;
+    column-gap: 2rem;
+    row-gap: 0.5rem;
+  }
+
+  .btn-block {
+    align-self: end;
+    margin-top: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    .btn-block {
+      margin-top: 0;
+    }
+  }
+`;
+
+export default StyledSearchContainer;
