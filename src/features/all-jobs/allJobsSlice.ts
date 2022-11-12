a49646c034f58c1ef8ba79248547d974c832f243
Bonamente <56506035@protonmail.com>
2022-11-12 10:25:55 +0500

    feat(allJobsSlice): add changePage action

--- src/features/all-jobs/allJobsSlice.ts
+++ src/features/all-jobs/allJobsSlice.ts
@@ -43,6 +43,9 @@ const allJobsSlice = createSlice({
     clearFilters: (state) => {
       return { ...state, ...initialFiltersState };
     },
+    changePage: (state, { payload }) => {
+      state.page = payload;
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -74,7 +77,12 @@ const allJobsSlice = createSlice({
   },
 });
 
-export const { showLoading, hideLoading, handleChange, clearFilters } =
-  allJobsSlice.actions;
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  changePage,
+  clearFilters,
+} = allJobsSlice.actions;
 
 export default allJobsSlice.reducer;
