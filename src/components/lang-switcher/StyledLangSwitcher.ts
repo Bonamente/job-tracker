273309f1223b7415086346b8c781d8b8ca726f34
Bonamente <56506035@protonmail.com>
2022-11-18 10:12:14 +0500

    refactor(StyledLangSwitcher): delete excess code

--- src/components/lang-switcher/StyledLangSwitcher.ts
+++ src/components/lang-switcher/StyledLangSwitcher.ts
@@ -1,21 +1,6 @@
 import styled from 'styled-components';
 
 const StyledLangSwitcher = styled.div`
-  .btn {
-    display: block;
-    margin: 0;
-    padding: 0;
-
-    font: inherit;
-    text-align: center;
-
-    appearance: none;
-    border: none;
-    background-color: transparent;
-
-    cursor: pointer;
-  }
-
   .lang-switch-btn {
     position: relative;
 
@@ -27,10 +12,7 @@ const StyledLangSwitcher = styled.div`
 
     font-size: 16px;
     font-weight: 700;
-    line-height: 1;
-    color: #ffffff;
 
-    background-color: #2563eb;
     border-radius: 50%;
   }
 `;
