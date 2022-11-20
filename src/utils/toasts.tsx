ae6bf8d079a4b767f36118c75fc2a56dc72d4daf
Bonamente <56506035@protonmail.com>
2022-11-20 18:48:11 +0500

    feat: add toast messages

---
+++ src/utils/toasts.tsx
@@ -0,0 +1,11 @@
+import { i18nextInstance as i18n } from './i18n';
+
+export const unauthorizedError = () => i18n.t('toasts.unauthorized');
+export const jobCreatedMsg = () => i18n.t('toasts.job_created');
+export const jobModifiedMsg = () => i18n.t('toasts.job_modified');
+export const userUpdated = () => i18n.t('toasts.user_updated');
+export const error = () => i18n.t('toasts.error');
+export const greeting = (name: string) =>
+  `${i18n.t('toasts.greeting')}${name}!`;
+export const welcomeBack = (name: string) =>
+  `${i18n.t('toasts.welcome_back')}${name}!`;
