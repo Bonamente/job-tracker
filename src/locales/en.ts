18685ef5f7c4cb35faa73c26824d25c83b661f14
Bonamente <56506035@protonmail.com>
2022-11-20 18:43:25 +0500

    feat: add english locale

---
+++ src/locales/en.ts
@@ -0,0 +1,143 @@
+export default {
+  translation: {
+    titles: {
+      main_page: 'Job <0>Tracking</0> App',
+      navbar: 'Dashboard',
+      charts: 'Monthly applications',
+      search_form: 'Search form',
+      add_job: 'Add job',
+      edit_job: 'Edit job',
+      profile: 'Profile',
+      no_job: 'There are no jobs to display.',
+      pagination: 'Pagination',
+      not_found: 'Sorry, page not found',
+    },
+
+    theme: {
+      activate: 'activate',
+      dark: 'dark',
+      light: 'light',
+      mode: 'mode',
+    },
+
+    mainPage: {
+      description: `The purpose of this app is to make the job search process more
+      convenient. Add jobs that you have applied for, track the progress
+      of each of them, and watch monthly statistics on the number of
+      applications submitted.`,
+      wish_text: 'Happy job hunting!',
+    },
+
+    signupPage: {
+      no_account: 'Not a member yet?',
+      has_account: 'Already a member?',
+      signin: 'Sign In',
+      signup: 'Sign Up ',
+    },
+
+    notFound: {
+      text: 'The page you requested could not be found',
+      link: 'Back home',
+    },
+
+    links: {
+      'add job': 'add job',
+      'all jobs': 'all jobs',
+      profile: 'profile',
+      stats: 'stats',
+    },
+
+    stats: {
+      pending: 'Pending applications',
+      interviews: 'Interviews scheduled',
+      declined: 'Jobs declined',
+    },
+
+    charts: {
+      area: 'area',
+      bar: 'bar',
+      chart: 'chart',
+      count: 'count',
+      show: 'Show',
+      bar_chart: 'Bar Chart',
+      area_chart: 'Area Chart',
+    },
+
+    form: {
+      name: 'Name',
+      email: 'Email',
+      company: 'Company',
+      location: 'Location',
+      password: 'Password',
+      position: 'Position',
+      'last name': 'Last name',
+      'job location': 'Job location',
+      'job type': 'Job type',
+      search: 'Search',
+      sort: 'Sort',
+      status: 'Status',
+      type: 'Type',
+    },
+
+    selectOption: {
+      all: 'all',
+      remote: 'remote',
+      internship: 'internship',
+      declined: 'declined',
+      interview: 'interview',
+      pending: 'pending',
+      latest: 'latest',
+      oldest: 'oldest',
+      'part-time': 'part-time',
+      'full-time': 'full-time',
+      'a-z': 'a-z',
+      'z-a': 'z-a',
+    },
+
+    job_type: {
+      remote: 'Remote',
+      internship: 'Internship',
+      'part-time': 'Part-time',
+      'full-time': 'Full-time',
+    },
+
+    status_type: {
+      declined: 'declined',
+      interview: 'interview',
+      pending: 'pending',
+    },
+
+    jobs_found: {
+      job_one: '{{count}} job found',
+      job_other: '{{count}} jobs found',
+    },
+
+    buttons: {
+      clear: 'clear',
+      clear_filters: 'clear filters',
+      delete: 'delete',
+      edit: 'edit',
+      loading: 'loading...',
+      login: `sign in / sign up`,
+      logout: 'sign out',
+      submit: 'submit',
+      go_to: 'Go to page',
+      prev: 'prev',
+      next: 'next',
+      wait: 'please wait...',
+      save: 'save changes',
+    },
+
+    toasts: {
+      all_fields: 'Please fill out all fields',
+      job_created: 'Job created',
+      job_modified: 'Job modified',
+      user_updated: 'User updated!',
+      sign_out: 'Signing out...',
+      unauthorized: 'Unauthorized! Signing out...',
+      error: 'There was an error',
+      greeting: 'Hi, ',
+      welcome_back: 'Welcome back, ',
+    },
+  },
+};
