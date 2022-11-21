7e20317d12725d62f26d54957448d41b8754145b
Bonamente <56506035@protonmail.com>
2022-11-21 10:00:11 +0500

    refactor: initI18n function

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,12 +1,16 @@
+/* eslint-disable import/no-mutable-exports */
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import resources from '../locales';
 
+// to get direct access to the instance of i18n, so as not to use Trans component or useTranslation hook.
+export let i18nextInstance: typeof i18n;
+
 const initI18n = async () => {
   const currentLanguage = localStorage.getItem('currentLang') || 'ru';
 
-  const i18nextInstance = i18n.createInstance();
+  i18nextInstance = i18n.createInstance();
   await i18nextInstance.use(initReactI18next).init({
     lng: currentLanguage,
     resources,
