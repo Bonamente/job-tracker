8cacd6fb229e9eac2f3c5ab81b6412bb095bc89c
Bonamente <56506035@protonmail.com>
2022-11-23 12:12:55 +0500

    feat(NotFoundPage): add translation

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,17 +1,22 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import StyledNotFoundPage from './StyledNotFoundPage';
 import img from '../../assets/images/not-found.svg';
 
-const NotFoundPage = () => (
-  <StyledNotFoundPage className="full-page">
-    <main>
-      <img src={img} alt="not found" />
-      <h1>Sorry, Page Not Found</h1>
-      <p>The page you requested could not be found</p>
-      <Link to="/">back home</Link>
-    </main>
-  </StyledNotFoundPage>
-);
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledNotFoundPage className="full-page">
+      <main>
+        <img src={img} alt="not found" />
+        <h1>{t('titles.not_found')}</h1>
+        <p>{t('notFound.text')}</p>
+        <Link to="/">{t('notFound.link')}</Link>
+      </main>
+    </StyledNotFoundPage>
+  );
+};
 
 export default NotFoundPage;
