dd07d5a12ae04f16c4f2451627f1bb92724b38ce
Bonamente <56506035@protonmail.com>
2022-11-24 10:57:05 +0500

    feat(StatsContainer): add translation

--- src/components/stats-container/StatsContainer.tsx
+++ src/components/stats-container/StatsContainer.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-array-index-key */
+import { useTranslation } from 'react-i18next';
 import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
 
 import StyledStatsContainer from './StyledStatsContainer';
@@ -6,25 +7,26 @@ import StatsItem from '../stats-item/StatsItem';
 import { useAppSelector } from '../../hooks/redux-hooks';
 
 const StatsContainer = () => {
+  const { t } = useTranslation();
   const { stats } = useAppSelector((store) => store.allJobs);
 
   const defaultStats = [
     {
-      title: 'pending applications',
+      title: t('stats.pending'),
       count: stats.pending || 0,
       icon: <FaSuitcaseRolling />,
       color: '#e9b949',
       bgColor: '#fcefc7',
     },
     {
-      title: 'interviews scheduled',
+      title: t('stats.interviews'),
       count: stats.interview || 0,
       icon: <FaCalendarCheck />,
       color: '#647acb',
       bgColor: '#e0e8f9',
     },
     {
-      title: 'jobs declined',
+      title: t('stats.declined'),
       count: stats.declined || 0,
       icon: <FaBug />,
       color: '#d66a6a',
