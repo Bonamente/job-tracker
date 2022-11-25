d395ee46f924286c810dea32756cacd044243199
Bonamente <56506035@protonmail.com>
2022-11-25 09:26:10 +0500

    feat(Select): add translation

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/no-array-index-key */
+import { useTranslation } from 'react-i18next';
+
 type SelectProps = {
   labelText?: string; // Specify later
   name: string;
@@ -13,25 +15,29 @@ const Select: React.FC<SelectProps> = ({
   value,
   list,
   handleChange,
-}) => (
-  <div className="form-row">
-    <label className="form-label" htmlFor={name}>
-      {labelText || name}
-    </label>
-    <select
-      className="form-select"
-      id={name}
-      name={name}
-      value={value}
-      onChange={handleChange}
-    >
-      {list.map((itemVal, idx) => (
-        <option key={idx} value={itemVal}>
-          {itemVal}
-        </option>
-      ))}
-    </select>
-  </div>
-);
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="form-row">
+      <label className="form-label" htmlFor={name}>
+        {labelText ? t(`form.${labelText}`) : t(`form.${name}`)}
+      </label>
+      <select
+        className="form-select"
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+      >
+        {list.map((itemVal, idx) => (
+          <option key={idx} value={itemVal}>
+            {t(`selectOption.${itemVal}`)}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default Select;
