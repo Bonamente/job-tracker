03eabd1beaba71cd15eb2c1a31fc5fb826562ac6
Bonamente <56506035@protonmail.com>
2022-12-04 11:05:39 +0500

    refactor: update customFetch function

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { getUserFromLocalStorage } from './localStorage';
 
 const customFetch = axios.create({
-  baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 customFetch.interceptors.request.use((config) => {
