c910cdd46542625cf0c731035e5150d6d86e8a88
Bonamente <56506035@protonmail.com>
2022-12-09 10:06:52 +0500

    feat: update setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,11 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
 
+import * as ResizeObserverModule from 'resize-observer-polyfill';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(global as any).ResizeObserver = ResizeObserverModule.default;
+
 expect.extend(matchers);
 
 afterEach(() => {
