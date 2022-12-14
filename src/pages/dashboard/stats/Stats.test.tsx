a767b2d6baf2e157af795368f6ae5dc039ec6258
Bonamente <56506035@protonmail.com>
2022-12-14 10:28:10 +0500

    test(Stats): add test

---
+++ src/pages/dashboard/stats/Stats.test.tsx
@@ -0,0 +1,61 @@
+import { renderWithProviders, screen, waitFor } from 'src/test/test-utils';
+import { vi } from 'vitest';
+import initI18n from 'src/utils/i18n';
+import { setupServer } from 'msw/node';
+import handlers from 'src/test/server-handlers';
+import * as reduxHooks from 'react-redux';
+import Stats from './Stats';
+
+vi.mock('react-redux');
+const mockedSelector = vi.spyOn(reduxHooks, 'useSelector');
+
+describe('Stats page', () => {
+  beforeAll(() => {
+    initI18n();
+  });
+
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('renders the Stats page with ChartsContainer', async () => {
+    const server = setupServer(...handlers);
+    server.listen();
+
+    renderWithProviders(<Stats />);
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole('heading', { name: /interviews scheduled/i })
+      ).toBeInTheDocument();
+    });
+
+    expect(
+      screen.getByRole('heading', { name: /monthly applications/i })
+    ).toBeInTheDocument();
+
+    server.resetHandlers();
+    server.close();
+  });
+
+  it('renders the Stats page without ChartsContainer if there are no monthly applications', async () => {
+    mockedSelector.mockReturnValue({
+      isLoading: false,
+      monthlyApplications: [],
+    });
+
+    renderWithProviders(<Stats />);
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole('heading', { name: /interviews scheduled/i })
+      ).toBeInTheDocument();
+    });
+
+    expect(
+      screen.queryByRole('heading', { name: /monthly applications/i })
+    ).not.toBeInTheDocument();
+
+    mockedSelector.mockClear();
+  });
+});
