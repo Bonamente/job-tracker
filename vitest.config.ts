b95ca52e71bc444f6c347a4eadf3c5ec209033b9
Bonamente <56506035@protonmail.com>
2022-12-18 10:43:36 +0500

    chore: add vitest.config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { mergeConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      exclude: [...configDefaults.exclude, 'e2e-tests/*', 'tests-examples/*'],
+    },
+  })
+);
