805d0db035ef9ddaabab48af09106f50e7f0b069
Bonamente <56506035@protonmail.com>
2022-12-21 13:14:27 +0500

    feat(toasts): add internationalization for job removed message

--- src/utils/toasts.tsx
+++ src/utils/toasts.tsx
@@ -3,6 +3,7 @@ import { i18nextInstance as i18n } from './i18n';
 export const unauthorizedError = () => i18n.t('toasts.unauthorized');
 export const jobCreatedMsg = () => i18n.t('toasts.job_created');
 export const jobModifiedMsg = () => i18n.t('toasts.job_modified');
+export const jobRemovedMsg = () => i18n.t('toasts.job_removed');
 export const userUpdated = () => i18n.t('toasts.user_updated');
 export const error = () => i18n.t('toasts.error');
 export const greeting = (name: string) =>
